44aa6b0c4d0072187f98491d1b94889057b70d2c
chris kenyon <62793208+chriskenyon1991@users.noreply.github.com>
2020-11-03 09:46:23 +0000

    add login page to app through router

--- skillshare/src/App.js
+++ skillshare/src/App.js
@@ -1,10 +1,24 @@
- import './App.css';
- import React from 'react'
+import './App.css';
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Login from './pages/login'
 
 function App() {
   return (
     <div className="App">
-      <h1>hello world</h1>
+      <h1>in app.js</h1>
+      <Router>
+        <Switch>
+          <Route path="/">
+            <Login/>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ skillshare/src/pages/login.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+class Login extends React.Component{
+
+
+    render(){
+        return(
+            <h1>in login page</h1>
+        )
+    }
+}
+
+export default Login
