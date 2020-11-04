c60af61642b43619bae7515e2bf932e73f4894eb
psd90 <pdhillon.dev@gmail.com>
2020-11-04 12:32:41 +0000

    old merge conflict

--- skillshare/src/pages/home.jsx
+++ skillshare/src/pages/home.jsx
@@ -1,19 +1,153 @@
-import React from 'react'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import axios from "axios";
 
-class Home extends React.Component{
+class Home extends React.Component {
+  state = {
+    currentUser: "alex",
+    showFilters: true,
+    categories: [],
+    wantsCategories: [],
+    teachCategories: [],
+    results: [],
+  };
 
+  toggleFilters = (e) => {
+    e.preventDefault();
+    this.setState((previousState) => {
+      return {
+        showFilters: !previousState.showFilters,
+      };
+    });
+  };
 
-    render(){
-        return(
-            <h1>in home page</h1>
-        )
+  renderFilters = () => {
+    if (this.state.showFilters) {
+      return (
+        <>
+          <h2>
+            Search for people by their &quot;wants to learn&quot; skills by
+            category
+          </h2>
+          {Object.keys(this.state.categories).map((category, index) => {
+            return (
+              <label key={index} htmlFor={category}>
+                {category}
+                <input
+                  type="checkbox"
+                  name={category}
+                  className="wantsCategories"
+                  onClick={this.toggleCategories}
+                />
+              </label>
+            );
+          })}
+          <h2>
+            Search for people by their &quot;can teach&quot; skills by category
+          </h2>
+          {Object.keys(this.state.categories).map((category, index) => {
+            return (
+              <label key={index} htmlFor={category}>
+                {category}
+                <input
+                  type="checkbox"
+                  name={category}
+                  className="teachCategories"
+                  onClick={this.toggleCategories}
+                />
+              </label>
+            );
+          })}
+        </>
+      );
     }
+  };
+
+  toggleCategories = (e) => {
+    const filterType = e.target.className;
+    const category = e.target.name;
+    if (this.state[filterType].includes(category)) {
+      const indexOfCategory = this.state[filterType].indexOf(category);
+      this.state[filterType].splice(indexOfCategory, 1);
+    } else {
+      this.state[filterType].push(category);
+    }
+    console.log(
+      `Currently selected ${filterType} filters: ${this.state[filterType]}`
+    );
+  };
+
+  componentDidMount() {
+    axios
+      .get("https://firebasing-testing.firebaseio.com/skills.json")
+      .then((res) => {
+        this.setState({ categories: res.data }, () => {
+          console.log("the categores in state are:", this.state.categories);
+        });
+      });
+  }
+
+  renderResults = (e) => {
+    e.preventDefault();
+
+    const { wantsCategories, categories } = this.state;
+    const skillPromises = [];
+    /*Categories are saved in state - we need to access the skills in each category,
+        and then make individual calls to the desired_skills node to find everyone who wants to learn the skills in that category */
+    wantsCategories.forEach((category) => {
+      const skills = [...Object.keys(categories[category])];
+      skills.forEach((skill) => {
+        skillPromises.push(
+          axios.get(
+            `https://firebasing-testing.firebaseio.com/desired_skills.json?orderBy="$key"&equalTo="${skill}"`
+          )
+        );
+      });
+    });
+    Promise.all(skillPromises).then((resArr) => {
+      /*We now have the names of everyone who wants to learn the skills in the category selected in the search.
+      Now we need to take their names (or keys) and find matching users in the user table to get the rest of their information. Again an individual call for each name is required*/
+      const dataArr = resArr.map((res) => res.data);
+      const userPromises = [];
+      const names = [];
+      dataArr.forEach((skill) => {
+        for (const prop in skill) {
+          names.push(...Object.keys(skill[prop]));
+        }
+      });
+      var uniqueNames = names.filter((v, i, a) => a.indexOf(v) === i);
+      uniqueNames.forEach((name) => {
+        userPromises.push(
+          axios.get(
+            `https://firebasing-testing.firebaseio.com/users.json?orderBy="$key"&equalTo="${name}"`
+          )
+        );
+      });
+      // Now we have all of the users' data as an array, we need to render it on the page
+      Promise.all(userPromises).then((resArr) => {
+        const dataArr = resArr.map((res) => res.data);
+        console.log(dataArr);
+      });
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <form action="">
+          <label htmlFor="searchBar">
+            Search:
+            <input name="searchBar" type="text" />
+          </label>
+          <button onClick={this.toggleFilters}>Filter</button>
+          {this.renderFilters()}
+          <br />
+          <br />
+          <button onClick={this.renderResults}>Search</button>
+        </form>
+      </>
+    );
+  }
 }
 
-export default Home
+export default Home;
