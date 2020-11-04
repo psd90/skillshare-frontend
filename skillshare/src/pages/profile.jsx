137a3d7561bab598011d0d1fff2e6e76de67e74c
berbakay <rick.kay90@gmail.com>
2020-11-04 11:43:19 +0000

    add stars rating and list of skills to profile page

--- skillshare/src/pages/profile.jsx
+++ skillshare/src/pages/profile.jsx
@@ -26,16 +26,46 @@ class Profile extends React.Component{
             const teacherVoteCounts = res.data.teacher_ratings.length; 
             res.data.student_ratings = {average: studentAverage, total: studentVoteCounts};
             res.data.teacher_ratings = {average: teacherAverage, total: teacherVoteCounts};
-            console.log(res.data)
-          this.setState({user: res.data, isLoading: false});
-        });
+            return res.data
+        })
+        .then(user => {
+            const desiredSkills = axios.get(`https://firebasing-testing.firebaseio.com/users_desired_skills/${username}.json`)
+            return Promise.all([user, desiredSkills])
+        })
+        .then(([user, desiredSkills]) => {
+            const teachingSkills = axios.get(`https://firebasing-testing.firebaseio.com/users_teaching_skills/${username}.json`)
+            return Promise.all([user, desiredSkills.data, teachingSkills ])
+        })
+        .then(([user, desiredSkills, teachingSkills]) => {
+            this.setState({user, desiredSkills, teachingSkills: teachingSkills.data, isLoading: false})
+        })
     }
 
     render(){
-        console.log(this.state)
         if(this.state.isLoading) return (<p>loading...</p>)
+        const teacherStars = [];
+        const studentStars = [];
+        let teacherRating = Math.round(this.state.user.teacher_ratings.average);
+        let studentRating = Math.round(this.state.user.student_ratings.average);
+        for (let i = 0; i < 5; i++) {
+            if(teacherRating > 0) {
+                teacherStars.push('★')
+                teacherRating--;
+            } else {
+                teacherStars.push('☆')
+            }
+            if(studentRating > 0) {
+                studentStars.push('★')
+                studentRating--;
+            } else {
+                studentStars.push('☆')
+            }
+        }
         return(
             <div id="profile-page">
+                <div id="profile-add-friend-button-div">
+                    <button className="profile-add-friend-button">Add Friend</button>
+                </div>
                 <div id="brief-user-data">
                     <img id='profile-image' src={this.state.user.photoUrl} alt={`${this.state.user.name}'s Profile Picture`}/>
                     <p>@{this.props.match.params.username}</p>
@@ -45,14 +75,37 @@ class Profile extends React.Component{
                 <div id="profile-ratings">
                     <div id="profile-teacher-ratings">
                         <h3>TEACHER</h3>
-                        <p>{this.state.user.teacher_ratings.average}</p>
+                        <p className="profile-stars">{teacherStars}</p>
+                        {/* <p>{this.state.user.teacher_ratings.average}</p> */}
                         ({this.state.user.teacher_ratings.total} reviews)
                     </div>
                     <div id="profile-student-ratings">
                         <h3>STUDENT</h3>
-                        <p>{this.state.user.student_ratings.average}</p>
+                        <p className="profile-stars">{studentStars}</p>
                         <p>({this.state.user.student_ratings.total} reviews)</p>
+                    </div>  
+                </div>
+                <h2>My Skillset</h2>
+                <div id="skills-list">
+                    <div id ="teaching-skills">
+                        <h3>My Skills</h3>
+                        <ul id="teaching-skills-list">
+                        {Object.keys(this.state.teachingSkills).map(skill => {
+                            return <p key={skill}>{skill}</p>
+                        })}
+                        </ul>
                     </div>
+                    <div id="desired-skills">
+                        <h3>Desired Skills</h3>
+                        <ul id = 'desired-skills-list'>
+                        {Object.keys(this.state.desiredSkills).map(skill => {
+                            return <p key={skill}>{skill}</p>
+                        })}
+                        </ul>
+                    </div>
+                </div>
+                <div id="profile-send-message-button-div">
+                    <button className="profile-send-message-button">Send Message</button>
                 </div>
             </div>
         )
