ba64fc5b43c909d8d1c8a80d78130389a4d0c0cc
Alex Brown <macuser@Macs-iMac.local>
2020-11-05 11:48:39 +0000

    half way through form validation

---
+++ skillshare/src/FormValidation.js
@@ -0,0 +1,8 @@
+// console.log("hello");
+// const usernameInput = document.getElementById("username");
+
+// usernameInput.addEventListener("change", (event) => {
+//   console.dir(event.target.value);
+//   const value = event.target.value;
+//   const username = event.target.username;
+// });
--- skillshare/src/pages/signup.jsx
+++ skillshare/src/pages/signup.jsx
@@ -3,6 +3,23 @@ import { withRouter } from "react-router";
 import app from "../base";
 import PropTypes from "prop-types";
 
+// usernameInput.addEventListener("change", (event) => {
+//   console.dir(event.target.value);
+//   const value = event.target.value;
+//   const name = event.target.name;
+//   const regex = /^[A-Za-z\-]+$/g;
+//   const isValid = regex.test(value);
+//   if (!isValid) {
+//     const warningText = document.createElement("p");
+//     warningText.classList.add("warning-text");
+//     warningText.textContent = "Username must contain characters a-z or '-'";
+//     const container = document.querySelector(`#${name}-container`);
+//     container.appendChild(warningText);
+//   } else {
+//     event.target.classList.add("valid");
+//   }
+// });
+
 const SignUp = ({ history }) => {
   const handleSignUp = useCallback(
     async (event) => {
@@ -24,18 +41,34 @@ const SignUp = ({ history }) => {
     history.push("/login");
   };
 
+  let passUsername = "i am a string";
+
+  const usernameChange = (event) => {
+    const username = event.target.value;
+    const regex = /^[0-9A-Za-z\-]+$/g;
+    const isValid = regex.test(username);
+    if (!isValid) {
+      passUsername = false;
+    } else {
+      passUsername = true;
+    }
+  };
+
   return (
     <>
       <h1 className="signUpHeading">Sign Up</h1>
       <div className="formcard">
         <div className="innerForm">
-          <form className="signUpForm" onSubmit={handleSignUp}>
+          <form className="signUpForm" id="form" onSubmit={handleSignUp}>
             <input
+              onChange={usernameChange}
               className="signUpInput"
               name="username"
+              id="username"
               type="text"
               placeholder="Username"
             />
+            <passUsername />
             <input
               className="signUpInput"
               name="email"
@@ -68,5 +101,26 @@ const SignUp = ({ history }) => {
 SignUp.propTypes = {
   history: PropTypes.node,
 };
-
 export default withRouter(SignUp);
+
+// const formElements = document.getElementById("form");
+// console.log(formElements);
+// let usernamePass = false;
+// let emailPass = false;
+// let passwordPass = false;
+// let datePass = false;
+
+// // for (let i = 0; i < formElements.length; i++) {
+// //   const obj = {
+// //     username: "change",
+// //     email: "change",
+// //     password: "keyup",
+// //     date: "change",
+// //     submit: "click",
+// //   };
+// //   const eventToUse = obj[formElements[i].name];
+// //   formElements[i].addEventListener(eventToUse, (event) => {
+// //     const value = event.target.value;
+// //     const name = event.target.name;
+// //   });
+// // }
